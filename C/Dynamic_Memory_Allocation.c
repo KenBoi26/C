151e07ed858c0ebaa690bc7d25c504a8a6a5ca2c
KenBoi26 <kennethphilip23@lpu.in>
2024-04-24 20:42:54 +0530

    updated calloc notes

--- C/Dynamic_Memory_Allocation.c
+++ C/Dynamic_Memory_Allocation.c
@@ -1,7 +1,45 @@
+/*
+// Malloc
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
     int *ptr;
-    
+
+    // using malloc function to define the size of block in bytes
+    // And making our ptr point to this newly created address space
+
+    ptr = malloc(sizeof(int));
+
+    if(ptr!=NULL){
+        printf("Memory is created using the malloc() function");
+    }else{
+        printf("Memory is not created");
+    }
+
+    return 0;
+}
+*/
+
+
+
+// Calloc
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *ptr;
+
+    // using calloc function to define the size of block in bytes
+    // And making our ptr point to this newly created address space
+
+    ptr = calloc(4, sizeof(int));
+
+    if(ptr!=NULL){
+        printf("Memory is created successfully");
+    }else{
+        printf("Memory is not created");
+    }
+
+    return 0;
 }
