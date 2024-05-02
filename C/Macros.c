9b0eb28908aaa5ec11159874770cfaa58fd6ee03
KenBoi26 <kennethphilip23@lpu.in>
2024-05-02 14:01:39 +0530

    Add macro examples in C

---
+++ C/Macros.c
@@ -0,0 +1,54 @@
+// Object-like Macros
+/*
+#include <stdio.h>
+
+#define NUM 26
+
+int main(){
+
+    printf("The value of NUM is %d\n",NUM);
+
+    return 0;
+}
+
+
+// Chain Macros
+
+#include <stdio.h>
+
+#define KENNETH DOB
+#define DOB 2005
+
+int main(){
+
+    printf("The DOB of Kenneth is: %d\n", KENNETH);
+
+    return 0;
+}
+
+
+// Multi-line Macros
+
+#include <stdio.h>
+
+#define SCORES 50, \
+                51, \
+                52, \
+                53
+
+int main(){
+
+    int arr[] = {SCORES};
+
+    for(int i=0; i<4; i++){
+        printf("The value of arr[%d] is %d\n",i,arr[i]);
+    }
+    
+    
+    return 0;
+}
+*/
+
+// Function-Like Macros
+
+#include <stdio.h>
